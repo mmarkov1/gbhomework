0ce3c572cbfc73337b6f42beeb5da97c8f34c809
Максим Марков <legkiyprocces@mail.ru>
2021-11-23 02:58:15 +0500

    Добавлены все задания

---
+++ less2/3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            if (Convert.ToInt32(Console.ReadLine()) % 2 == 0) Console.WriteLine("Четное");
+            else Console.WriteLine("Нечетное");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ less2/4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Для полного закрепления понимания простых типов найдите любой чек, либо фотографию этого чека в интернете и схематично нарисуйте его в консоли,
+//только за место динамических, по вашему мнению, данных (это может быть дата, название магазина, сумма покупок) подставляйте переменные,
+//которые были заранее заготовлены до вывода на консоль.
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int busNumber = 41;
+            int cost = 20;
+            int ticketNumber = 0015;
+            string series = "14000 - 712831037 - 555";
+            Console.WriteLine("Автобусный билет");
+            Console.WriteLine("");
+            Console.WriteLine("билет № " + ticketNumber);
+            Console.WriteLine("Серия № " + series);
+            Console.WriteLine("Время  " + DateTime.Now);
+            Console.WriteLine("Стоимость  " + cost);
+            Console.WriteLine("Номер автобуса  " + busNumber);
+
+            Console.ReadLine();
+
+        }
+    }
+}
